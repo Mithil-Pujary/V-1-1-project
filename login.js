b100f605726df95e1b8bf24e7b514ef3150e8a33
Nevil12345 <165174315+Nevil12345@users.noreply.github.com>
2024-12-27 13:19:52 +0530

    js

---
+++ login.js
@@ -0,0 +1,17 @@
+// do everything  only when the doument is loded
+$(function(e){
+    //capture the keyup event
+    $(document).on("keyup","input",function(e){
+        let un=$("username").val();
+        let pw=$("password").val();
+        if(un.trim()!=="" && pw.trim()!=="")
+        {
+            $("#btllogin").removeClass("inactivecolor");
+            $("#btllogin").addClass("activecolor");
+        }
+        else{
+            $("#btllogin").removeClass("activecolor");
+            $("#btllogin").addClass("inactivecolor");
+        }
+    });
+});
