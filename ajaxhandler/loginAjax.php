17c3a14f6e5d3f3a6d6176b1837bdc6d7bcce127
Mithil-Pujary <mithilpoojari151@gmail.com>
2024-12-27 17:35:54 +0530

    Updated alot of things

---
+++ ajaxhandler/loginAjax.php
@@ -0,0 +1,32 @@
+<?php
+$path=$_SERVER['DOCUMENT_ROOT'];
+require_once $path."/attendanceapp/database/database.php";
+require_once $path."/attendanceapp/database/faculty_details.php";
+$action=$_REQUEST["action"];
+
+if(!empty($action))
+{
+    if($action=="verifyuser")
+    {
+        //retrieve what was sent
+        $un=$_POST["username"];
+        $pw=$_POST["password"];
+
+        $dbobj=new Database();
+
+        $fdo= new faculty_Details();
+
+        $rv=$fdo->verifyuser($dbobj,$un,$pw);
+
+        if($rv['status']=="ALL OK")
+        {
+            session_start();
+            $_SESSION['current_user']=$rv['fac_id'];
+        }
+        else
+        {
+            echo json_encode($rv);
+        }
+    }
+}
+?>
